cdda4b5f6a44db6f8ffdfadf8006a57050fc1b05
Ivan Hribernik <cihribernik@gmail.com>
2024-02-04 00:27:12 -0300

    initial navbar

--- services/frontend/src/components/NavBar.vue
+++ services/frontend/src/components/NavBar.vue
@@ -1,59 +1,78 @@
 <template>
-  <v-app :full-height="false" class="app-bar-container">
-    <v-app-bar :elevation="2">
-      <!-- eslint-disable-next-line vue/valid-v-on -->
-      <v-app-bar-nav-icon @click="drawer = !drawer" id="hamburger-icon">
-      </v-app-bar-nav-icon>
-      <v-app-bar-title>Pluton</v-app-bar-title>
-    </v-app-bar>
-    <v-navigation-drawer v-model="drawer" app>
-      <v-list>
-        <v-list-item
-          v-for="item in menuItems"
-          :key="item.title"
-          @click="notdefinedyet">
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </v-app>
+  <v-app-bar app>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-toolbar-title class="block-title" color="purple darken-2">
+      <v-icon class="negative-logo">
+        <v-img :src="logo" max-height="24" max-width="24"></v-img>
+      </v-icon>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn :to="'/login'" v-if="!isLoggedIn">Login</v-btn>
+    <v-btn :to="'/register'" v-if="!isLoggedIn"> Registre </v-btn>
+    <v-btn @click="logout" variant="elevated" v-if="isLoggedIn">Logout</v-btn>
+  </v-app-bar>
+
+  <v-navigation-drawer app v-model="drawer" permanent>
+    <v-list>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="headline">Menu</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+
+      <v-list-item
+        v-for="(item, index) in menuItems"
+        :key="index"
+        :to="item.route">
+        <v-list-item-icon>
+          <v-icon>
+            {{ item.icon }}
+          </v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>
+            {{ item.title }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
-<script>
-import { defineComponent, ref } from 'vue';
+<script setup>
+import { ref, computed } from 'vue';
+// eslint-disable-next-line
+import logo from '@/assets/pluton-logo.png';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
 
-export default defineComponent({
-  name: 'NavBar',
-  setup() {
-    const drawer = ref(false);
-    const menuItems = [
-      { title: 'Listado de resultados' },
-      { title: 'Carga de resultados' },
-      { title: 'My Profile' },
-      { title: 'Log-out' },
-    ];
+const store = useStore();
+const router = useRouter();
 
-    return {
-      drawer,
-      menuItems,
-    };
-  },
-  computed: {
-    isLoggedIn: function () {
-      return this.$store.getters.isAuthenticated;
-    },
-  },
-  methods: {
-    async logout() {
-      await this.$store.dispatch('logOut');
-      this.$router.push('/login');
-    },
-  },
-});
+const drawer = ref(true);
+
+const menuItems = [
+  { title: 'Listado de resultados', route: '/dashboard', logged: true },
+  { title: 'Carga de resultados', route: '/dashboard', logged: true },
+  { title: 'My Profile', route: '/profile', logged: true },
+];
+
+const isLoggedIn = computed(() => store.getters.isAuthenticated);
+
+const logout = async () => {
+  await store.dispatch('logOut');
+  await router.push('/login');
+};
 </script>
 
 <style scoped>
-a {
-  cursor: pointer;
+.block-title {
+  display: flex;
+  align-items: center;
+}
+
+.negative-logo img {
+  filter: invert(100%);
 }
 </style>
